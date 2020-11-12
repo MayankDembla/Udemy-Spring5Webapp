3a1f6a36689b18fe63601c9b294c819523c4123e
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-11-13 01:15:34 +0530

    Section 2 : Adding Slides of Solid Principals

--- src/main/java/com/ayan/spring/webapp/services/SetterBasedGreetingsService.java
+++ src/main/java/com/ayan/spring/webapp/services/SetterBasedGreetingsService.java
@@ -3,7 +3,7 @@ package com.ayan.spring.webapp.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SetterBasedGreetingsController implements GreetingService {
+public class SetterBasedGreetingsService implements GreetingService {
 
 
     @Override
