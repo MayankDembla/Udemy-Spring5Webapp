8c3bcf09c56d86b3b535f20841e4c5de2f20f674
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-11-16 06:02:37 +0530

    Try to create Bean using XML based Config..

--- src/main/java/com/ayan/spring/webapp/WebappApplication.java
+++ src/main/java/com/ayan/spring/webapp/WebappApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.ayan.spring")
+@ImportResource("classpath:chuck-config.xml")
 public class WebappApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ayan/spring/webapp/config/ChuckNorrisQuotesConfig.java
+++ src/main/java/com/ayan/spring/webapp/config/ChuckNorrisQuotesConfig.java
@@ -1,15 +1,11 @@
 package com.ayan.spring.webapp.config;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
+// @Configuration
 public class ChuckNorrisQuotesConfig {
 
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes() {
-        return new ChuckNorrisQuotes();
-    }
+//    @Bean
+//    public ChuckNorrisQuotes chuckNorrisQuotes() {
+//        return new ChuckNorrisQuotes();
+//    }
 
 }
