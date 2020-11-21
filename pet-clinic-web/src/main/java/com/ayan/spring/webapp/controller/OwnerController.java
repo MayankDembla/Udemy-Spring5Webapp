40aa79d4f5b263ef5be73abe3f5d5af23c77b1d0
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-11-22 00:54:33 +0530

    Getting Pet List and Owener List

--- pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/OwnerController.java
@@ -1,6 +1,8 @@
 package com.ayan.spring.webapp.controller;
 
+import com.ayan.spring.webapp.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OwnerController {
 
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String getOwner() {
+    public String getOwner(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "/owner/index";
     }
 
