cfaf541e11cb3e400c439b69d08c4cbf12fd6fa0
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-11-28 05:58:48 +0530

    Add Specialities and Vet having multiple specialities

---
+++ pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/Speciality.java
@@ -0,0 +1,9 @@
+package com.ayan.spring.webapp.model;
+
+import lombok.Data;
+
+@Data
+public class Speciality extends BaseEntity {
+
+    String description;
+}
--- pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/Vet.java
@@ -1,4 +1,12 @@
 package com.ayan.spring.webapp.model;
 
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
 }
