59a9dc3347e10d01cae04eaa7bc57d62316f4fbc
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-02 15:58:52 +0530

    Ingredient and UnitofMeasure - One to One Relationship

--- src/main/java/com/ayan/spring/webapp/domain/UnitOfMeasure.java
+++ src/main/java/com/ayan/spring/webapp/domain/UnitOfMeasure.java
@@ -3,23 +3,17 @@ package com.ayan.spring.webapp.domain;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 
-@Entity
 @Data
-public class Ingredient {
+@Entity
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
-    private BigDecimal amount;
 
-    @ManyToOne
-    private Recipe recipe;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure unitOfMeasure;
-
+    @OneToOne
+    private Ingredient ingredient;
 }
