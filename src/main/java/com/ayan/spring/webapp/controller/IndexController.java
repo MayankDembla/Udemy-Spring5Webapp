7bd6c91d4aeac764757f144c6708a7d8be0fcbeb
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-02 19:25:21 +0530

    Data JPA Query Methods

--- src/main/java/com/ayan/spring/webapp/controller/IndexController.java
+++ src/main/java/com/ayan/spring/webapp/controller/IndexController.java
@@ -1,16 +1,38 @@
 package com.ayan.spring.webapp.controller;
 
+import com.ayan.spring.webapp.domain.Category;
+import com.ayan.spring.webapp.domain.UnitOfMeasure;
+import com.ayan.spring.webapp.repositories.CategoryRepository;
+import com.ayan.spring.webapp.repositories.UnitofMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitofMeasureRepository unitofMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitofMeasureRepository unitofMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitofMeasureRepository = unitofMeasureRepository;
+    }
+
 
     @GetMapping(value = {"", "/", "/index"})
-    public static String getindexPage() {
-        System.out.println("Hello buddy");
+    public String getindexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitofMeasureRepository.findByDescription("Pinch");
+
+        System.out.println("Cat Id is " + categoryOptional.get().getId());
+        System.out.println("UM ID id : " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
+
+
 
 }
--- src/main/java/com/ayan/spring/webapp/repositories/UnitofMeasureRepository.java
+++ src/main/java/com/ayan/spring/webapp/repositories/UnitofMeasureRepository.java
@@ -3,5 +3,9 @@ package com.ayan.spring.webapp.repositories;
 import com.ayan.spring.webapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitofMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+   Optional<UnitOfMeasure> findByDescription(String description);
 }
