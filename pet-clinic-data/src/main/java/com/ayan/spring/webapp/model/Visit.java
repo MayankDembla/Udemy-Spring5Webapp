8bc434352f1c1dbae06d187dc20482c7d64805f0
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-07 13:21:46 +0530

    Convert the Visit to Entity

--- pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/Visit.java
+++ pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/Visit.java
@@ -3,14 +3,23 @@ package com.ayan.spring.webapp.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate localDate;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_visit")
     private Pet pet;
 
 }
