25f23dac991a71f1214cc72a4c365c95e2f8023f
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-07 12:44:29 +0530

    @MappedSuperclass - using base Entity

--- pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ayan/spring/webapp/model/BaseEntity.java
@@ -5,8 +5,10 @@ import lombok.Data;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @Data
+@MappedSuperclass // Hey JPA we are going to inherit this class to other classes
 public class BaseEntity {
 
     @Id
