936a258f4a6c6542019a24c54a4038458603c04f
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-07 18:46:40 +0530

    Spring Data JPA for Pet Service

---
+++ pet-clinic-data/src/main/java/com/ayan/spring/webapp/services/springdatajpa/PetJPAService.java
@@ -0,0 +1,48 @@
+package com.ayan.spring.webapp.services.springdatajpa;
+
+import com.ayan.spring.webapp.model.Pet;
+import com.ayan.spring.webapp.repositories.PetRepository;
+import com.ayan.spring.webapp.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().iterator().forEachRemaining(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
