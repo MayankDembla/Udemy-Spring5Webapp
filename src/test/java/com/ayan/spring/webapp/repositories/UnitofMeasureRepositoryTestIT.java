977524b0aa0e081fd7903a3f6dccd408cbe36a3c
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-11 17:12:21 +0530

    @DirtiesContext use

--- src/test/java/com/ayan/spring/webapp/repositories/UnitofMeasureRepositoryTestIT.java
+++ src/test/java/com/ayan/spring/webapp/repositories/UnitofMeasureRepositoryTestIT.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -25,6 +26,7 @@ public class UnitofMeasureRepositoryTestIT {
     }
 
     @Test
+    @DirtiesContext
     public void findByDescription() {
 
         Optional<UnitOfMeasure> ofMeasure = unitofMeasureRepository.findByDescription("Teaspoon");
