7e583d71e3271b2c6ee222e5f6eafe1f288bff28
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-11 16:53:11 +0530

    Adding the Test Cases in Controller, model and Service... Using Argument Capture to get the Argument and verify it.

---
+++ src/test/java/com/ayan/spring/webapp/controller/IndexControllerTest.java
@@ -0,0 +1,69 @@
+package com.ayan.spring.webapp.controller;
+
+import com.ayan.spring.webapp.domain.Recipe;
+import com.ayan.spring.webapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getindexPage() {
+
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(2L);
+
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        // When
+        String viewName = indexController.getindexPage(model);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+
+        //   First parameter is equal to recipe and return any set
+//        verify(model, times(1)).addAttribute(eq("recipes") , anySet()) ;
+
+
+        // Use Argument Captor
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
+    }
+}
