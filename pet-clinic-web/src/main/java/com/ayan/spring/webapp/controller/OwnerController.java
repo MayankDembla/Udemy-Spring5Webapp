5d696088d7f5f60e3481c91220164718c38280bc
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-12-26 12:52:36 +0530

    Implement WebData Binder - @InitBinder

--- pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/OwnerController.java
@@ -3,7 +3,9 @@ package com.ayan.spring.webapp.controller;
 import com.ayan.spring.webapp.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,11 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/owners")
 public class OwnerController {
 
-
     private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder webDataBinder) {
+        webDataBinder.setAllowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
