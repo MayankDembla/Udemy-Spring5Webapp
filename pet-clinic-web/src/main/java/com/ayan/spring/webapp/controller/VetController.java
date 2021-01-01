35dd6bcac9d4b7990176e7ada9fa11f0ab46eec8
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2021-01-01 14:47:59 +0530

    Return JSON for Vets using @ResponseBody Annotation

--- pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/ayan/spring/webapp/controller/VetController.java
@@ -1,9 +1,14 @@
 package com.ayan.spring.webapp.controller;
 
+import com.ayan.spring.webapp.model.Vet;
 import com.ayan.spring.webapp.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,12 +19,18 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html", "vet.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html", "vet.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "/vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
