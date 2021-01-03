b62200a66333713ee4fb5b2f60668556155615d8
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2021-01-04 02:54:24 +0530

    Adding the Bootstrap for SQL - dev and prod data

--- src/main/java/com/ayan/spring/webapp/bootstrap/RecipeBootStrap.java
+++ src/main/java/com/ayan/spring/webapp/bootstrap/RecipeBootStrap.java
@@ -6,6 +6,7 @@ import com.ayan.spring.webapp.repositories.RecipeRepository;
 import com.ayan.spring.webapp.repositories.UnitofMeasureRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootStrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final RecipeRepository recipeRepository;
